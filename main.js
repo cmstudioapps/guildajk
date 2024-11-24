e8615ade350cb54e0c56e1a637e9103cb54bbaea
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-11-24 12:53:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+let feed = document.getElementById("feed");
+
+const url = "https://escola-97256-default-rtdb.firebaseio.com/guilda/.json";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    let htmlContent = "";
+
+    // Converta o objeto em um array de posts, incluindo o postId para manter a referência
+    const postsArray = Object.keys(data).map(postId => ({ id: postId, ...data[postId] }));
+
+    // Ordene os posts pela data mais recente (ano, mês, dia, hora e minuto)
+    postsArray.sort((a, b) => {
+      const dateA = new Date(a.Ano, a.Mes - 1, a.Dia, a.Hora, a.Minuto);
+      const dateB = new Date(b.Ano, b.Mes - 1, b.Dia, b.Hora, b.Minuto);
+      return dateB - dateA; // Ordem decrescente
+    });
+
+    // Gere o HTML na ordem desejada
+    postsArray.forEach(post => {
+      
+      
+      htmlContent = `
+        <div class="post">
+          <img src="${post.IMG}" alt="Imagem do post" height="400px">
+          <p>${post.LG}</p>
+          <p id='id' style='font-size:10pt;'>id: ${post.id}</p>
+       <a id='ll' href="${post.Link}" target='_blank' style='display: ${post.visivel};'>Saiba mais</a>
+        </div>
+      `;
+      
+    });
+    
+    
+if (!htmlContent.includes("script")) {
+  
+    feed.innerHTML = htmlContent;
+}
+  })
+  .catch(error => {
+    console.error("Erro ao buscar dados:", error);
+    alert("ocorreu um erro na exibição de dados, verifique sua internet mas se continuar reporte o problema aos desenvolvedores.")
+  });
+  
+  
+  function entrarGuilda() {
+    
+    
+    
+    
+  }
---
+++ pontos.js
@@ -0,0 +1,68 @@
+let data = new Date 
+let dia = data.getDate()
+let diaAnterior = localStorage.getItem ("diaAnterior")
+let compartilhou = Number(localStorage.getItem("compartilhou"))
+if (dia != diaAnterior) {
+  
+  pontos = pontos +2
+  localStorage.setItem("pontos",pontos)
+  atualizarPontos()
+  
+  diaAnterior = dia
+  localStorage.setItem("diaAnterior",diaAnterior)
+
+if(localStorage.getItem("compartilhou") === 1 || !localStorage.getItem("compartilhou") ) {
+compartilhou = 0
+localStorage.setItem("compartilhou",compartilhou)
+}
+  
+  
+}
+
+
+function shar() {
+  
+  if(navigator.share) {
+  
+  navigator.share({
+    
+    title: "Guilda JK",
+    text: "https://guildajk.vercel.app"
+    
+    
+    
+  })
+  .then(data => {
+    
+    if(localStorage.getItem("compartilhou") == 0) {
+      
+      compartilhou = 1
+      localStorage.setItem("compartilhou",compartilhou)
+      
+      pontos = pontos +2
+      
+      localStorage.setItem("pontos",pontos)
+      
+      atualizarPontos()
+      
+      
+      alert("Obrigado por compartilhar! vc ganhou 2 pontos!")
+      
+      
+    } else {
+      
+      
+      alert("Obrigado por compartilhar!")
+      
+    }
+    
+    
+  })
+  } else {
+    
+    alert("Seu navegador não é compatível")
+    
+    
+  }
+  
+}
