f1fccc721f9b0e836e80217c2609266b1611293d
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-11-26 16:23:52 -0300

    Atualizar o Date.js

--- Date.js
+++ Date.js
@@ -2,3 +2,20 @@ let data = new Date
 let dia = data.getDate()
 let diaAnterior = localStorage.getItem ("diaAnterior")
 let compartilhou = Number(localStorage.getItem("compartilhou"))
+
+if (dia != diaAnterior) {
+
+  pontos = pontos +2
+  localStorage.setItem("pontos",pontos)
+  atualizarPontos()
+
+  diaAnterior = dia
+  localStorage.setItem("diaAnterior",diaAnterior)
+
+if(localStorage.getItem("compartilhou") == 1 || !localStorage.getItem("compartilhou") ) {
+compartilhou = 0
+localStorage.setItem("compartilhou",compartilhou)
+}
+  localStorage.removeItem("clicouSaiba")
+
+}
